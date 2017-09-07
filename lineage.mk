e54ff703ac3b42367ee2a36c2c17869e2a093885
ih24n69 <muhammadihsan69@gmail.com>
2017-09-07 18:34:40 +0700

    switch to LOS

--- lineage.mk
+++ lineage.mk
@@ -1,13 +1,11 @@
 #
-# Copyright (C) 2016 Android Open Source Project
 # Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2017 The LineageOS Project SM-G355H by Ih24n
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-# http://www.apache.org/licenses/LICENSE-2.0
+#      http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -16,13 +14,16 @@
 # limitations under the License.
 #
 
-# This file is executed by build/envsetup.sh, and can use anything
-# defined in envsetup.sh.
-#
-# In particular, you can add lunch options with the add_lunch_combo
-# function: add_lunch_combo generic-eng
+# Inherit device configuration
+$(call inherit-product, $(LOCAL_PATH)/device_kanas3g.mk)
 
-for targetBuildVariant in eng user userdebug
-do
-  add_lunch_combo lineage_kanas3g-${targetBuildVariant}
-done
+# Override build date
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
+
+# Device identifier
+PRODUCT_DEVICE := kanas3g
+PRODUCT_NAME := lineage_kanas3g
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-G355H
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_CHARACTERISTICS := phone
